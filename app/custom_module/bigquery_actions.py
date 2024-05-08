de37946d005c5055c6386d12392ee14db8504b68
chrisb <chrism.budnik@gmail.com>
2024-05-08 19:18:25 +0200

    feature: send data to bigquery func

--- app/custom_module/bigquery_actions.py
+++ app/custom_module/bigquery_actions.py
@@ -2,7 +2,10 @@ import logging
 from typing import Literal
 import pandas as pd
 from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
 from fastapi import HTTPException
+from fastapi.responses import JSONResponse
+
 
 # BigQuery actions logger
 logger = logging.getLogger(__name__)
@@ -15,11 +18,12 @@ def dataframe_to_bigquery(
         schema: list[bigquery.SchemaField] = None,
         partition_field: str = None,
         clustering_fields: list[str] = None,
-        write_disposition: Literal["WRITE_TRUNCATE", "WRITE_APPEND", "WRITE_EMPTY"] = "WRITE_APPEND",
+        write_disposition: Literal[
+            "WRITE_TRUNCATE", "WRITE_APPEND", "WRITE_EMPTY"
+            ] = "WRITE_APPEND",
     ) -> Literal["export-completed", "missing-data"]:
     """
 
-    
     """
     # If empty - warning of missing data
     if data.empty: 
@@ -57,3 +61,54 @@ def dataframe_to_bigquery(
         raise HTTPException(status_code=500, detail="Failed to upload data to BigQuery.")
 
     return "export-completed"
+    
+
+def check_if_table_exists(
+        client: bigquery.Client, dataset_id: str, table_id: str
+    ) -> bool:
+    """
+    Check if a table exists in BigQuery using the full table ID.
+
+    Args:
+    client (bigquery.Client): The BigQuery client.
+    dataset_id (str): The dataset ID.
+    table_id (str): The table ID.
+
+    Returns:
+    bool: True if the table exists, False otherwise.
+    """
+    try:
+        full_table_id = f"{client.project}.{dataset_id}.{table_id}"
+        client.get_table(full_table_id)  
+        return True
+    
+    except NotFound as e:
+        logger.error(f"Table '{full_table_id}' not found in BigQuery. {e}")
+        return False
+    
+def send_data_to_bigquery(
+        client: bigquery.Client, 
+        dataset_id: str, 
+        table_id: str, 
+        data: dict
+    ) -> JSONResponse:
+
+    # Check if the table exists in dataset
+    if not check_if_table_exists(client, dataset_id, table_id):
+        raise HTTPException(status_code=404, detail="Table not found in BigQuery.")
+    
+    # Create a table reference, and insert the data
+    table_ref = client.dataset(dataset_id).table(table_id)
+    errors = client.insert_rows_json(table_ref, [data])  
+    
+    # Return errors if any
+    if errors:
+        logger.error(f"Errors while inserting into BigQuery: {errors}")
+        raise HTTPException(status_code=404, detail="Failed to send data to BigQuery.")
+    
+    else:
+        logger.info("Data sent to BigQuery successfully.")
+        return JSONResponse({"message": "Data sent to BigQuery successfully."}, status_code=200)
+
+
+
